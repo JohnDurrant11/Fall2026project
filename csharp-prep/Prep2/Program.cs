815ba84f939eb2b6cac9582b89dbcfa8969391a6
John Durrant <jdurrantwork@gmail.com>
2026-09-24 18:56:44 -0600

    Made a grade tracker to inform the user if they have passed or failed based upon what their grade percentage was.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        String grade_input = Console.ReadLine();
+        int  grade_Percentage = int.Parse(grade_input);
+        String grade_Letter;
+        String pass_Status;
+
+        if (grade_Percentage >= 90)
+            {
+                grade_Letter = "A";
+            }
+            else if (grade_Percentage >= 80)
+            {
+                grade_Letter = "B";
+            }
+            else if (grade_Percentage >= 70)
+            {
+                grade_Letter = "C";
+            }
+            else if (grade_Percentage >= 60)
+            {
+                grade_Letter = "D";
+            }
+            else
+            {
+                grade_Letter = "F";
+            }
+
+        if (grade_Percentage >= 70)
+        {
+            pass_Status = "Congragulations on passing!";
+        }
+        else
+        {
+            pass_Status = "Try again next time.";
+        }
+
+        Console.WriteLine($"{grade_Letter} {pass_Status}");
     }
 }
