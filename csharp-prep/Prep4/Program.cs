13d39f4801dcfba352f1898e233e4f2034485538
John Durrant <jdurrantwork@gmail.com>
2026-09-25 22:18:56 -0600

    Made code that gets user input and applies it to a list which then displays the sum, average and greatest number of said list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string user_Add = "";
+        int user_Number = -1;
+        int sum_Number = 0;
+        int largest_Number = 0;
+        List<int> added_Numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, enter 0 when finished.");
+
+        while (user_Number != 0)
+        {
+            Console.Write("Enter a number: ");
+            user_Add = Console.ReadLine();
+            user_Number = int.Parse(user_Add);
+            added_Numbers.Add(user_Number);
+        }
+        added_Numbers.RemoveAt(added_Numbers.Count -1);
+
+        foreach (int number in added_Numbers)
+        {
+            sum_Number = sum_Number + number;
+        }
+        Console.WriteLine($"The sum is: {sum_Number}");
+
+        int average_Number = sum_Number / added_Numbers.Count;
+        Console.WriteLine($"The average is: {average_Number}");
+
+        foreach (int number in added_Numbers)
+        {
+            if (number > largest_Number)
+            {
+                largest_Number = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest_Number}");
     }
 }
